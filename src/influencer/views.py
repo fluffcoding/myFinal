278d05f3af8d0faf1dd507073997b0d17c231318
fluffcoding <m.mashood@khi.iba.edu.pk>
2020-08-25 16:06:12 +0500

    Added new views and templates

--- src/influencer/views.py
+++ src/influencer/views.py
@@ -23,3 +23,25 @@ def campaign_detail(request):
 
 def memer_assignment(request):
     return render(request, 'memer/list.html', {})
+
+
+def memer_detail(request):
+    return render(request, 'memer/detail.html', {})
+    
+
+def memer_dashboard(request):
+    return render(request, 'memer/dashboard.html', {})
+
+
+def influencer_dashboard(request):
+    return render(request, 'influencer/dashboard.html', {})
+
+
+def login_view(request):
+    return render(request, 'user/login.html', {})
+
+
+def register_view(request):
+    return render(request, 'user/register.html', {})
+
+    
